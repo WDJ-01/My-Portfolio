b9d99d77ca2ef0c59eefcba84f4168190489bf5b
Wian De Jager <104436216+WDJ-01@users.noreply.github.com>
2023-02-24 14:40:28 +0200

    remove webpack comments

--- webpack.config.js
+++ webpack.config.js
@@ -4,33 +4,34 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   output: {
-    path: path.join(__dirname, "/dist"), // the bundle output path
-    filename: "bundle.js", // the name of the bundle
+    path: path.join(__dirname, "/dist"), 
+    filename: "bundle.js", 
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "public/index.html", // to import index.html file inside index.js
+      template: "public/index.html", 
     }),
 
   ],
   devServer: {
-    port: 3030, // you can change the port
+ 
+    port: 3030, 
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/, // .js and .jsx files
-        exclude: /node_modules/, // excluding the node_modules folder
+        test: /\.(js|jsx)$/, 
+        exclude: /node_modules/, 
         use: {
           loader: "babel-loader",
         },
       },
       {
-        test: /\.(css|scss)$/, // styles files
+        test: /\.(css|scss)$/, 
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
         loader: "url-loader",
         options: { limit: false },
       },
